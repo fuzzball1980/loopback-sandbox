af60908fd7452a004e904f7e05dbefbe4f9d9170
fuzzball1980 <cesar@alttab.com.ar>
2016-06-04 17:32:51 +0000

    check issue https://github.com/strongloop/loopback/issues/2400

---
+++ common/models/process-magazine.js
@@ -0,0 +1,47 @@
+module.exports = function(ProcessMagazine) {
+
+  var loopback = require('loopback');
+  var app = require('../../server/server');
+
+  ProcessMagazine.beforeRemote('**', function(ctx, instance, next) {
+    // TODO: check issue https://github.com/strongloop/loopback/issues/2400
+    console.log(ctx.methodString, JSON.stringify(ctx.req.body));
+
+    next();
+  });
+
+  ProcessMagazine.getMagazineFileInfo = function(issue, cb) {
+    var error = new Error();
+    error.statusCode = 500;
+    error.message = "We have problems in our servers, try again later";
+    error.stack = "Unknow Error";
+
+    // TODO: check issue https://github.com/strongloop/loopback/issues/2400
+    console.log('Issue: ' + JSON.stringify(issue));
+
+    var retVal = {};
+    cb(null, retVal);
+  }
+
+  ProcessMagazine.remoteMethod(
+    'getMagazineFileInfo', {
+      description: 'Get PDF File metadata from and uploaded issue.',
+      accepts: [{
+        arg: 'issue',
+        type: 'Issue',
+        description: 'Get Magazine PDF metadata',
+        http: {
+          source: 'body'
+        },
+        required: true
+      }],
+      returns: {
+        arg: 'response',
+        type: 'object'
+      },
+      http: {
+        verb: 'post'
+      }
+    }
+  );
+};
--- server/boot/buitin-models-mixins.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app) {                                                              
-   app.models.ACL.mixin('Audit');
-   app.models.RoleMapping.mixin('Audit');
-   app.models.Role.mixin('Audit');
-   // app.models.AuthProvider.mixin('Audit');
-};
